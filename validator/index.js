5516ca06ad9bdc07df2cd3f2697223da41afc711
Saman Soltani <sam.soltani@gmail.com>
2020-08-20 23:53:20 +0200

    improve data validator

--- validator/index.js
+++ validator/index.js
@@ -13,8 +13,9 @@ var valid_model = validate_model(schema_model);
 console.log('validating the model:', valid_model)
 if (!valid_model) console.log(validate_model.errors);
 
-
-var validate_data = ajv.compile(schema_model);
-var valid_data = validate_data(data);
-console.log('validating data:' , valid_data)
-if (!valid_data) console.log(validate_data.errors);
+if (valid_model) {
+    var validate_data = ajv.compile(schema_model);
+    var valid_data = validate_data(data);
+    console.log('validating data:', valid_data)
+    if (!valid_data) console.log(validate_data.errors);
+}
