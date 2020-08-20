9c61a3f41ba4f28c3884b60249300b51884c9165
Saman Soltani <sam.soltani@gmail.com>
2020-08-20 23:48:02 +0200

    add validator

---
+++ validator/index.js
@@ -0,0 +1,20 @@
+var Ajv = require('ajv');
+
+var schema = require('../schema.json');
+var schema_model = require('../sending-email.asm.json');
+var data = require('../example.json');
+
+
+var ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+
+
+var validate_model = ajv.compile(schema);
+var valid_model = validate_model(schema_model);
+console.log('validating the model:', valid_model)
+if (!valid_model) console.log(validate_model.errors);
+
+
+var validate_data = ajv.compile(schema_model);
+var valid_data = validate_data(data);
+console.log('validating data:' , valid_data)
+if (!valid_data) console.log(validate_data.errors);
